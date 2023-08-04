fa8c340c1203c6ca769bcbb1e752f261aac23339
Hamza Waheed <hamzawaheed057@gmail.com>
2023-08-04 17:06:50 +0500

    Update

--- JS/index.js
+++ JS/index.js
@@ -1,16 +1,47 @@
 let string = '';
 let buttons = document.querySelectorAll('.btn');
 let phoneNumber = document.querySelector('.input');
+let clear = document.querySelector('.clearBtn')
+let callButton = document.querySelector('.callBtn')
 
 Array.from(buttons).forEach((button)=>{
     button.addEventListener('click', (x)=>{
-        x.stopPropagation();
-        console.log(x.target);
-        string += button.dataset.value;
-        updatePhoneNumber();
+            x.stopPropagation();
+            console.log(x.target);
+            string += button.dataset.value;
+            updatePhoneNumber();
     });
 });
 
+clear.addEventListener('click', function(){
+    if(phoneNumber){
+        string = string.substring(0, string.length-1);
+        updatePhoneNumber();
+    }
+});
+callButton.addEventListener('click', function(){
+    if(phoneNumber){
+        dialNumber(phoneNumber);
+        phoneNumber = ''
+        updatePhoneNumber();
+    }
+});
+
+function dialNumber(phoneNumber) {
+    console.log(`Dialing ${phoneNumber}`);
+    addToCallHistory(phoneNumber, '0:30');
+};
+
+function addToCallHistory(number, duration) {
+    const callHistoryList = document.createElement('ul');
+    callHistoryList.classList.add('list-group');
+    const listItem = document.createElement('li');
+    listItem.classList.add('list-group-item');
+    listItem.innerText = `Number: ${number} - Duration: ${duration}`;
+    callHistoryList.appendChild(listItem);
+    const callHistoryContainer = document.querySelector('.container-fluid');
+    callHistoryContainer.appendChild(callHistoryList);
+  };
 function updatePhoneNumber(){
     phoneNumber.value = string;
 }
